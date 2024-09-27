067d38a470c03c777cca7c55cb866f5a7c66c4ea
Rithik2211 <rithikpranao22@gmail.com>
2024-09-27 09:27:12 +0530

    Initial Commit

---
+++ SampleSDK/SampleSDK.h
@@ -0,0 +1,18 @@
+//
+//  SampleSDK.h
+//  SampleSDK
+//
+//  Created by Rithik Pranao on 27/09/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleSDK.
+FOUNDATION_EXPORT double SampleSDKVersionNumber;
+
+//! Project version string for SampleSDK.
+FOUNDATION_EXPORT const unsigned char SampleSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleSDK/PublicHeader.h>
+
+
